0e3a961e2b80a604dad27ed78b9a472a94372755
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2022-11-29 22:55:04 +0800

    style: horizontally center app components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import RandomWord from "random-words";
+import { useState } from "react";
 import "./App.css";
+import HangmanDisplay from "./components/hangmanDisplay/HangmanDisplay";
 import InputDisplay from "./components/inputDisplay/InputDisplay";
 
 function App() {
+  const [word, setWord] = useState(RandomWord(1)[0].toUpperCase());
+
   return (
-    <div className="App">
-      <InputDisplay />
+    <div className="App stack-h-center">
+      <HangmanDisplay />
+      <InputDisplay word={word} />
     </div>
   );
 }
