0db16f4aee60af9c8f1d5376975283f09657d0cd
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2022-11-29 22:53:47 +0800

    feat: create hang device

---
+++ src/components/hangmanDisplay/stand/HangDevice.tsx
@@ -0,0 +1,11 @@
+const HangDevice = () => {
+  return (
+    <div id="hang-device">
+      <div id="rope" className="black-fill"></div>
+      <div id="overhead" className="black-fill"></div>
+      <div id="pole" className="black-fill"></div>
+    </div>
+  );
+};
+
+export default HangDevice;
---
+++ src/components/hangmanDisplay/stand/Stand.tsx
@@ -0,0 +1,12 @@
+import HangDevice from "./HangDevice";
+
+const Stand = () => {
+  return (
+    <div>
+      <HangDevice />
+      <div id="floor" className="black-fill"></div>
+    </div>
+  );
+};
+
+export default Stand;
