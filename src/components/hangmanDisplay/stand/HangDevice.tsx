a218fa17baca2bda6da65f4987db3d02413134b0
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2022-11-29 23:00:59 +0800

    fix: reorder components

--- src/components/hangmanDisplay/stand/HangDevice.tsx
+++ src/components/hangmanDisplay/stand/HangDevice.tsx
@@ -1,9 +1,9 @@
 const HangDevice = () => {
   return (
     <div id="hang-device">
-      <div id="rope" className="black-fill"></div>
-      <div id="overhead" className="black-fill"></div>
       <div id="pole" className="black-fill"></div>
+      <div id="overhead" className="black-fill"></div>
+      <div id="rope" className="black-fill"></div>
     </div>
   );
 };
