6edd0c25032bae0f5e48088373108b7946a1e9ce
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2022-11-29 22:54:06 +0800

    feat: create human

---
+++ src/components/hangmanDisplay/Human.tsx
@@ -0,0 +1,12 @@
+const Human = () => {
+  return (
+    <div id="human">
+      <div id="human-head"></div>
+      <div id="human-body" className="black-fill"></div>
+      <div id="human-arms" className="human-limbs"></div>
+      <div id="human-legs" className="human-limbs"></div>
+    </div>
+  );
+};
+
+export default Human;
