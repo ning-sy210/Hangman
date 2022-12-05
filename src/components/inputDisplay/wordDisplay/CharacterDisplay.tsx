6ce617b0924b5136891fc8afae2f6ab5cbceba79
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2022-12-05 22:05:32 +0800

    feat: render color of game word conditionally depnding on game state

--- src/components/inputDisplay/wordDisplay/CharacterDisplay.tsx
+++ src/components/inputDisplay/wordDisplay/CharacterDisplay.tsx
@@ -1,10 +1,43 @@
+import { useContext } from "react";
+import {
+  GameState,
+  GameStateContext,
+} from "../../../contexts/GameStateContext";
+
 type charDisplay = {
   letter: string;
   show: boolean;
 };
 
 const CharacterDisplay = ({ letter, show }: charDisplay) => {
-  return <div className={`vh-center char-display`}>{show && letter}</div>;
+  const gameState = useContext(GameStateContext);
+
+  const getCharDisplayColor = () => {
+    switch (gameState) {
+      case GameState.FAILED:
+        return "failed-display";
+
+      case GameState.SOLVED:
+        return "solved-display";
+
+      default:
+        return "";
+    }
+  };
+
+  const renderLetter = () => {
+    if (!show && gameState === GameState.ACTIVE) {
+      return null;
+    }
+
+    return letter;
+  };
+
+  return (
+    <div className={`vh-center char-display ${getCharDisplayColor()}`}>
+      {renderLetter()}
+    </div>
+  );
 };
 
 export default CharacterDisplay;
